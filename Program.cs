6e8b2b350d2d163c48dcc43de84054cc424493b9
subhrasmita-ojha <60036690+subhrasmita-ojha@users.noreply.github.com>
2020-01-27 20:41:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace Assignment
+{
+    public class Employee
+    {
+        public delegate void EmpAction(string[] msg, int i);
+        public event EmpAction Action;
+        public string[] s1 = new string[3];
+        public int i;
+
+        public void DoAction()
+        {
+            Action(s1, i);
+        }
+        private int Id;
+        private string emp_name;
+        private string dept_name;
+
+        public Employee(int Id, string emp_name, string dept_name)
+        {
+            this.Id = Id;
+            this.emp_name = emp_name;
+            this.dept_name = dept_name;
+
+        }
+
+        public int GetId(Employee E1)
+        {
+            i = 0;
+            s1[0] = MethodBase .GetCurrentMethod().Name;
+            E1.Action += new Employee.EmpAction(show_Action);
+            E1.DoAction();
+            return this.Id;
+
+        }
+
+        public string GetName(Employee E1)
+        {
+            i = 1;
+            s1[1] = MethodBase.GetCurrentMethod().Name;
+            E1.Action += new Employee.EmpAction(show_Action);
+            E1.Action -= new Employee.EmpAction(show_Action);
+            E1.DoAction();
+            return this.emp_name;
+        }
+
+        public string GetDepartmentName(Employee E1)
+        {
+            i = 2;
+            s1[2] = MethodBase.GetCurrentMethod().Name;
+            E1.Action += new Employee.EmpAction(show_Action);
+            E1.Action -= new Employee.EmpAction(show_Action);
+            E1.DoAction();
+            return this.dept_name;
+        }
+
+        public void show_Action(string[] msg, int i)
+        {
+            Console.WriteLine(msg[i] + "method is called");
+
+        }
+
+        public void set(int uid)
+        {
+            Id = uid;
+            Console.WriteLine("Updated Id :" + Id);
+        }
+
+        public void set(string name)
+        {
+            emp_name = name;
+            Console.WriteLine("Updated employee name:" + emp_name);
+
+        }
+        public void set(string name, int Id)
+        {
+            dept_name = name;
+            Console.WriteLine("Updated Department Name:" + dept_name);
+        }
+
+    }
+}
+    
---
+++ emp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assignment
+{
+    class emp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Employee Id");
+            int Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name of the Employee ");
+            string emp_name = Console.ReadLine();
+
+            Console.WriteLine("Enter Department of the Employee ");
+            string dept_name = Console.ReadLine();
+
+            Employee E1 = new Employee(Id, emp_name, dept_name);
+            int id = E1.GetId(E1);
+            Console.WriteLine("Employee Id:" + id);
+
+            string ename = E1.GetName(E1);
+            Console.WriteLine("Employee Name:" + ename);
+
+            string dname = E1.GetDepartmentName(E1);
+            Console.WriteLine("Department of the employee:" + dname);
+
+            string i = E1.GetName(E1);
+            Console.WriteLine("Enter Id to Update the Employee Id");
+            int update_id = Convert.ToInt32(Console.ReadLine());
+
+            E1.set(update_id);
+            Console.WriteLine("Enter name of employee to update the name");
+            string update_ename = Console.ReadLine();
+            E1.set(update_ename);
+            Console.WriteLine("Enter department name of  the employee to update the department name");
+            string update_dname = Console.ReadLine();
+            E1.set(update_dname, 0);
+            Console.ReadKey();
+
+
+        }   
+    }
+}
